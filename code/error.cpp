8a3369bfd6a8db13f94d1f1c88ac899160f8fafb
FanDingyi <1975804693@qq.com>
2023-03-13 21:06:05 +0800

    bool operator and preprocesser test

--- code/error.cpp
+++ code/error.cpp
@@ -1,4 +1,10 @@
+#include "error.cpp"
 int main(int argc,char** argv){
+	while(true && true){
+	//loop and bool test
+	;
+	}
+	  
 	//nothing happened here 
 	//nothing happened here 
 	//nothing happened here 
