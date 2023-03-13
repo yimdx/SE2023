f2a48e0068b5008b4f4d49e6f4e9efb12a5b6919
FanDingyi <1975804693@qq.com>
2023-03-13 20:59:31 +0800

    Repetition rate checking

--- code/error.cpp
+++ code/error.cpp
@@ -1,3 +1,27 @@
 int main(int argc,char** argv){
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
+	//nothing happened here 
 	return main(argc,argv);
 }
